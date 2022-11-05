93e4eb6d39d242bc6464c449bf40babc5acf3451
PavanChikkodikar <pavanchikkodikar@gmail.com>
2022-11-05 22:03:06 +0530

    first commit

---
+++ creating_node.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>  //standard input output
+#include <stdlib.h>  //for calling malloc function
+
+struct node{
+  int data;
+  struct node *link;
+};
+
+int main()
+{
+    struct node *head = NULL;
+    head = (struct node *)malloc(sizeof(struct node)); //storing the adress
+    
+    head->data=45; //for accessing the data
+    head->link=NULL;
+    
+    printf("%d", head->data);
+
+    return 0;
+}
